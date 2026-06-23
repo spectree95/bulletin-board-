2b22b1e524c103b486844f775a99806afa1b7851
Nurbol <beysenbaynurboo@gmail.com>
2026-06-23 21:20:23 +0500

    fix phone number

--- users/models.py
+++ users/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    phone_number = models.CharField(10)
+    phone_number = models.CharField(max_length=10)
     
     
