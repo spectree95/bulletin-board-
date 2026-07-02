ded828de6df975ac6b13cbbb7bbe9b3651f2f500
Nurbol <beysenbaynurboo@gmail.com>
2026-07-02 14:26:47 +0500

    vmesto default storage berem storages

--- bulletin_board/settings.py
+++ bulletin_board/settings.py
@@ -151,14 +151,16 @@ STATICFILES_DIRS = [
 WHITENOISE_AUTOREFRESH = True
 
 
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
-
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
 
 
 MEDIA_URL = '/media/'
