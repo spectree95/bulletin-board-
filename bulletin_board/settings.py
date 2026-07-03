e54fed968dc88fe87967f0852ff4cdda15c63b84
Nurbol <beysenbaynurboo@gmail.com>
2026-07-03 17:51:31 +0500

    logging

--- bulletin_board/settings.py
+++ bulletin_board/settings.py
@@ -151,6 +151,19 @@ STATICFILES_DIRS = [
 WHITENOISE_AUTOREFRESH = True
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
 
 
 STORAGES = {
