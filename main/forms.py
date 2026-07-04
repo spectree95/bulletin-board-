9c8619ac14c99dfa9ce32c3222ed19364f9156a0
Nurbol <beysenbaynurboo@gmail.com>
2026-07-04 15:28:35 +0500

    ochen tupaya owibka

--- main/forms.py
+++ main/forms.py
@@ -8,24 +8,24 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ["name", "price", "category", "subcategory"]
         
-        def clean(self):
-            cleaned_data = super().clean()
-            name = cleaned_data.get("name")
-            category = cleaned_data.get("category")
-            subcategory = cleaned_data.get('subcategory')
-            price = cleaned_data.get("price")
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        category = cleaned_data.get("category")
+        subcategory = cleaned_data.get('subcategory')
+        price = cleaned_data.get("price")
             
             
-            if name and len(name) < 8:
-                self.add_error("name", "Слишком короткое название.")
+        if name and len(name) < 8:
+            self.add_error("name", "Слишком короткое название.")
             
-            if subcategory and subcategory and subcategory.category != category:
-                raise forms.ValidationError("Подкатегория не принадлежит выбранной категории.")            
+        if subcategory and subcategory and subcategory.category != category:
+            raise forms.ValidationError("Подкатегория не принадлежит выбранной категории.")            
+        
+        if price and price < 0:
+            self.add_error("Цена должна быть больше 0.")
             
-            if price and price < 0:
-                self.add_error("Цена должна быть больше 0.")
-            
-            return cleaned_data
+        return cleaned_data    
         
         
    
